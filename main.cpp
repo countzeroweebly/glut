57045d22a4475f124bcc3317c7532e9666ab63cc
countzero <flank7er@prothonmail.com>
2015-07-13 07:23:48 +0400

    http://countzero.weebly.com/programming/opengl-gldrawarrays

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 /* Программа рисования вращающегося трехмерного куба
-с логотипом Slackware в альфа-канале. Компиляция:
-g++ -o ./имя_готовой_программы ./имя_этого_файла.cpp -lGL -lGLU -lglut -lpng
+с логотипом Slackware в альфа-канале.
+complie: g++ -o cube cube.cpp -lGL -lGLU -lglut -lpng -std=gnu++11
 */
 
 #include <iostream>
@@ -13,9 +13,10 @@ g++ -o ./имя_готовой_программы ./имя_этого_файла
 
 
 // именованные константы убранные из libpng-1.4
- #define png_infopp_NULL (png_infopp)NULL
+#define png_infopp_NULL (png_infopp)NULL
 #define int_p_NULL (int*)NULL
 #define png_voidp_NULL NULL
+
 
 // настройки тумана
 bool   gp;                      // G Pressed? ( New )
@@ -25,6 +26,7 @@ GLuint fogfilter= 0;                    // Which Fog To Use
 GLfloat fogColor[4]= {0.5f, 0.5f, 0.5f, 1.0f};      // Fog Color
 
 int fps=0;
+int counter=0;
 float angle=0;
 float elapsed_time,current_time;
 float last_time=0.0f;
@@ -32,6 +34,232 @@ bool flag=false;
 
 GLuint textureId;
 GLubyte *textureImage;
+
+ // вершинные координаты куба
+ float cube[]={
+ // Front face
+        -1.0f, -1.0f,  1.0f,
+         1.0f, -1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+        -1.0f,  1.0f,  1.0f,
+ // Back face
+        -1.0f, -1.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f,
+         1.0f,  1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+ // Top face
+        -1.0f,  1.0f, -1.0f,
+        -1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f, -1.0f,
+        // Bottom face
+        -1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f,  1.0f,
+        -1.0f, -1.0f,  1.0f,
+        // Right face
+         1.0f, -1.0f, -1.0f,
+         1.0f,  1.0f, -1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f, -1.0f,  1.0f,
+        // Left face
+        -1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f,  1.0f,
+        -1.0f,  1.0f,  1.0f,
+        -1.0f,  1.0f, -1.0f};
+
+ // текстурные координаты куба
+ float cubeTex[]={
+ // Front face 
+        0.0f, 0.0f,
+        1.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 1.0f,
+        // Back Face
+        1.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 1.0f,
+        0.0f, 0.0f,
+        // Top Face
+        0.0f, 1.0f,
+        0.0f, 0.0f,
+        1.0f, 0.0f,
+        1.0f, 1.0f,
+        // Bottom Face
+        1.0f, 1.0f,
+        0.0f, 1.0f,
+        0.0f, 0.0f,
+        1.0f, 0.0f,
+        // Right face
+        1.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 1.0f,
+        0.0f, 0.0f,
+        // Left Face
+        0.0f, 0.0f,
+        1.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 1.0f};
+
+// вывод текста на холст OpenGL посредством GLUT
+void draw_string_bitmap(void *font, const char* string) {
+        while (*string)
+                glutBitmapCharacter(font, *string++);
+
+}
+
+bool loadPngImage(char *name, int &outWidth, int &outHeight, bool &outHasAlpha, GLubyte **outData);
+
+void init() {
+        glClearColor(0.0, 0.0, 0.0, 1.0); // цвет очистки буферов - черный
+        glEnable(GL_DEPTH_TEST); // включаем сортировку по глубине
+        glShadeModel(GL_SMOOTH); // режим закрашивания: интерполируемые значения
+        // ставим источник света
+        float lightAmb[4] = {0.0, 0.0, 1.0, 1}; // фоновый цвет синий
+        float lightDif[4] = {1.0, 0.0, 0.0, 1}; // отраженный цвет красный
+        float lightPos[4] = {0.0, 3.0, 3.0, 1};  // положение в пространстве
+        glLightfv (GL_LIGHT0, GL_SPECULAR, lightAmb);
+        glLightfv (GL_LIGHT0, GL_DIFFUSE, lightDif);
+        glLightfv (GL_LIGHT0, GL_POSITION, lightPos);
+//        glEnable (GL_LIGHT0 ); // включаем источник света
+//        glEnable (GL_LIGHTING); // включаем освещение
+
+        glEnable(GL_BLEND); // включаем прозрачность
+        glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+        int width, height; // ширина и высота текстуры
+        bool hasAlpha;     // наличие альфа-канала
+
+        char filename[] = "cube_face.png"; // наша текстура
+        bool success = loadPngImage(filename, width, height, hasAlpha, &textureImage);
+
+        if (!success) {
+                std::cerr << "Невозможно прочитать PNG файл" << std::endl;
+                return;
+        }
+        std::cerr << "Изображение загружено: " << width << " " << height
+                                << " наличие  альфа-канала " << hasAlpha << std::endl;
+        glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // формат хранения текстуры в памяти
+        // добавляем нашу текстуру
+        glTexImage2D(GL_TEXTURE_2D, 0, hasAlpha ? 4 : 3, width,
+                        height, 0, hasAlpha ? GL_RGBA : GL_RGB, GL_UNSIGNED_BYTE, textureImage);
+        // параметры фильтрации текстуры
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+}
+
+void display() {
+        flag=true; // мы еще рисуем
+
+        // вычисляем время между отрисовкой двух кадров и по этому
+        // и используем это значение величину смещения
+        current_time=glutGet(GLUT_ELAPSED_TIME) / 10;
+        elapsed_time = current_time - last_time;
+        angle = ( angle >= 360.0f ) ? 0.0f : angle+elapsed_time;
+        last_time=current_time;
+
+        // ---- ОТРИСОВКА 3D ------------------
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+
+        glMatrixMode (GL_PROJECTION);
+        glLoadIdentity();
+
+        gluPerspective( 60, GLfloat(1.33f), 1, 20 );
+        // установка вектора обзора
+        // (0,0,3) камера; (0,0,0) центр сцены, верхом будет ось Y
+        gluLookAt( 0, 0, 3, 0, 0, 0, 0, 1, 0 );
+
+        glEnable (GL_LIGHT0 ); // включаем источник света
+        glEnable (GL_LIGHTING); // включаем освещение
+        glEnable(GL_TEXTURE_2D); // включаем использование текстур
+
+        glClear(GL_DEPTH_BUFFER_BIT|GL_COLOR_BUFFER_BIT); // очистка рабочих буферов
+        glPushMatrix(); // начинаем матричные преобразования
+        glTranslatef(0,0,-1); // смещение
+        glRotatef(angle, 1, 1, 0); // вращение
+
+        glClear(GL_COLOR_BUFFER_BIT); // очистка рабочих буферов
+        glEnableClientState(GL_VERTEX_ARRAY);
+        glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+        glVertexPointer(3,GL_FLOAT,0,cube);
+        glTexCoordPointer(2,GL_FLOAT,0,cubeTex);
+        glDrawArrays(GL_QUADS,0,24);
+        glDisableClientState(GL_VERTEX_ARRAY);
+        glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+        glPopMatrix(); // завершаем матричные преобразования
+
+        // ------ ОТРИСОВКА 2D ----------------------------------
+        glDisable(GL_TEXTURE_2D); // включаем использование текстур
+        glDisable (GL_LIGHT0 ); // выключаем источник света
+        glDisable (GL_LIGHTING); // выключаем освещение
+        glColor3f(1.0f,1.0f,1.0f);
+        glLoadIdentity();
+        gluOrtho2D(0, 400, 0, 300);
+        glRasterPos2f(300, 280);
+
+        std::string s="fps: " +std::to_string(counter);
+        const char *cstr = s.c_str();
+        draw_string_bitmap(GLUT_BITMAP_HELVETICA_18, cstr);
+
+
+        glutSwapBuffers(); // выводим на экран
+        flag=false;  // все, нарисовали!
+        fps++; // плюс один кадр
+}
+
+void reshape(int w, int h)
+{
+        // область видимости в соответствии с новыми координатами
+        glViewport( 0, 0, (GLsizei)w, (GLsizei)h);
+        // загружаем единичные матрицы в стеки MODELVIEW, PROJECTION
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        // установка перспективы: угол обзора 60 градусов, аспект,
+        // глубина пространства 20 единиц
+        gluPerspective( 60, GLfloat(1.33f), 1, 20 );
+        // установка вектора обзора
+        // (0,0,3) камера; (0,0,0) центр сцены, верхом будет ось Y
+        gluLookAt( 0, 0, 3, 0, 0, 0, 0, 1, 0 );
+}
+
+void key(unsigned char key, int  x, int y) {
+        // если была нажата клавиша ESC, Q, q, то завершаем программу
+        if ( key == 27 || key == 'q' || key == 'Q' ) {
+                glutLeaveMainLoop();
+        }
+}
+
+void mytime(int value) {
+        // выводим количество кадров которые успели нарисовать за 1 секунду
+//        std::cerr << "fps= " << fps << std::endl;
+ counter=fps;
+        fps=0; // обнуление счетчика
+        glutTimerFunc(1000, mytime, 0); // следующий вызов
+}
+
+int main(int argc, char** argv) {
+        glutInit (&argc,argv); // инициализация glut
+        // будем использовать двойную буферизацию, цветовое пространство RGB,
+        // сортировку по глубине aka Z-буфер
+        glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB|GLUT_DEPTH);
+        // размер окна пусть будет 800 на 600
+        glutInitWindowSize( 800,600);
+        // создаем окно, на выходе получаем идентификатор созданного окна
+        int glWin=glutCreateWindow("Slackware Cube");
+        init(); // подготовка к рисованию
+        glutDisplayFunc(display); // рисование
+        glutReshapeFunc(reshape); // действия при изменении окна
+        glutKeyboardFunc(key);   // обработка нажатий клавиш
+        // запускаем таймер м периодичностью 1 раз в секунду
+        glutTimerFunc(1000, mytime, 0);
+        glutIdleFunc(display); // анимация
+        glutMainLoop(); // главный цикл glut
+
+        return 0;
+}
 
 // загрузка png файла
 bool loadPngImage(char *name, int &outWidth, int &outHeight, bool &outHasAlpha, GLubyte **outData) {
@@ -135,161 +363,4 @@ bool loadPngImage(char *name, int &outWidth, int &outHeight, bool &outHasAlpha, 
 
     /* да, мы сделали это */
     return true;
-}
-
-
-void init() {
-        glClearColor(0.0, 0.0, 0.0, 1.0); // цвет очистки буферов - черный
-        glEnable(GL_DEPTH_TEST); // включаем сортировку по глубине
-        glShadeModel(GL_SMOOTH); // режим закрашивания: интерполируемые значения
-        // ставим источник света
-        float lightAmb[4] = {0.0, 0.0, 1.0, 1}; // фоновый цвет синий
-        float lightDif[4] = {1.0, 0.0, 0.0, 1}; // отраженный цвет красный
-        float lightPos[4] = {0.0, 3.0, 3.0, 1};  // положение в пространстве
-        glLightfv (GL_LIGHT0, GL_SPECULAR, lightAmb);
-        glLightfv (GL_LIGHT0, GL_DIFFUSE, lightDif);
-        glLightfv (GL_LIGHT0, GL_POSITION, lightPos);
-        glEnable (GL_LIGHT0 ); // включаем источник света
-        glEnable (GL_LIGHTING); // включаем освещение
-
-        glEnable(GL_BLEND); // включаем прозрачность
-        glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-        int width, height; // ширина и высота текстуры
-        bool hasAlpha;     // наличие альфа-канала
-
-        char filename[] = "image3.png"; // наша текстура
-        bool success = loadPngImage(filename, width, height, hasAlpha, &textureImage);
-
-        if (!success) {
-                std::cerr << "Невозможно прочитать PNG файл" << std::endl;
-                return;
-        }
-        std::cerr << "Изображение загружено: " << width << " " << height
-                                << " наличие  альфа-канала " << hasAlpha << std::endl;
-        glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // формат хранения текстуры в памяти
-        // добавляем нашу текстуру
-        glTexImage2D(GL_TEXTURE_2D, 0, hasAlpha ? 4 : 3, width,
-                        height, 0, hasAlpha ? GL_RGBA : GL_RGB, GL_UNSIGNED_BYTE, textureImage);
-        // параметры фильтрации текстуры
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glEnable(GL_TEXTURE_2D); // включаем использование текстур
-}
-
-void display() {
-        flag=true; // мы еще рисуем
-
-        // вычисляем время между отрисовкой двух кадров и по этому
-        // и используем это значение величину смещения
-        current_time=glutGet(GLUT_ELAPSED_TIME) / 10;
-        elapsed_time = current_time - last_time;
-        angle = ( angle >= 360.0f ) ? 0.0f : angle+elapsed_time;
-        last_time=current_time;
-
-        glClear(GL_DEPTH_BUFFER_BIT|GL_COLOR_BUFFER_BIT); // очистка рабочих буферов
-        glPushMatrix(); // начинаем матричные преобразования
-        glTranslatef(0,0,-1); // смещение
-        glRotatef(angle, 1, 1, 0); // вращение
-
-        glClearColor(0.5f,0.5f,0.5f,1.0f);          // We'll Clear To The Color Of The Fog ( Modified )
-        // рисуем туман
-        glFogi(GL_FOG_MODE, fogMode[fogfilter]);        // Fog Mode
-        glFogfv(GL_FOG_COLOR, fogColor);            // Set Fog Color
-        glFogf(GL_FOG_DENSITY, 0.35f);              // How Dense Will The Fog Be
-        glHint(GL_FOG_HINT, GL_DONT_CARE);          // Fog Hint Value
-        glFogf(GL_FOG_START, 3.0f);             // Fog Start Depth
-        glFogf(GL_FOG_END, -5.0f);               // Fog End Depth
-        glEnable(GL_FOG);                   // Enables GL_FOG
-
-        // рисуем куб с текстурой
-        glBegin(GL_QUADS);
-        // Front Face
-        glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);  // Bottom Left Of The Texture and Quad
-        glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);  // Bottom Right Of The Texture and Quad
-        glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  1.0f);  // Top Right Of The Texture and Quad
-        glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  1.0f);  // Top Left Of The Texture and Quad
-        // Back Face
-        glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);  // Bottom Right Of The Texture and Quad
-        glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);  // Top Right Of The Texture and Quad
-        glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);  // Top Left Of The Texture and Quad
-        glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);  // Bottom Left Of The Texture and Quad
-        // Top Face
-        glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);  // Top Left Of The Texture and Quad
-        glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f,  1.0f,  1.0f);  // Bottom Left Of The Texture and Quad
-        glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f,  1.0f,  1.0f);  // Bottom Right Of The Texture and Quad
-        glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);  // Top Right Of The Texture and Quad
-        // Bottom Face
-        glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, -1.0f, -1.0f);  // Top Right Of The Texture and Quad
-        glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f, -1.0f, -1.0f);  // Top Left Of The Texture and Quad
-        glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);  // Bottom Left Of The Texture and Quad
-        glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);  // Bottom Right Of The Texture and Quad
-        // Right face
-        glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);  // Bottom Right Of The Texture and Quad
-        glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);  // Top Right Of The Texture and Quad
-        glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  1.0f);  // Top Left Of The Texture and Quad
-        glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);  // Bottom Left Of The Texture and Quad
-        // Left Face
-        glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);  // Bottom Left Of The Texture and Quad
-        glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);  // Bottom Right Of The Texture and Quad
-        glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  1.0f);  // Top Right Of The Texture and Quad
-        glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);  // Top Left Of The Texture and Quad
-        glEnd();
-        glPopMatrix(); // завершаем матричные преобразования
-
-        glutSwapBuffers(); // выводим на экран
-        flag=false;  // все, нарисовали!
-        fps++; // плюс один кадр
-}
-
-void reshape(int w, int h)
-{
-        // область видимости в соответствии с новыми координатами
-        glViewport( 0, 0, (GLsizei)w, (GLsizei)h);
-        // загружаем единичные матрицы в стеки MODELVIEW, PROJECTION
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
-        // установка перспективы: угол обзора 60 градусов, аспект,
-        // глубина пространства 20 единиц
-        gluPerspective( 60, GLfloat(1.33f), 1, 20 );
-        // установка вектора обзора
-        // (0,0,3) камера; (0,0,0) центр сцены, верхом будет ось Y
-        gluLookAt( 0, 0, 3, 0, 0, 0, 0, 1, 0 );
-}
-
-void key(unsigned char key, int  x, int y) {
-        // если была нажата клавиша ESC, Q, q, то завершаем программу
-        if ( key == 27 || key == 'q' || key == 'Q' ) {
-                glutLeaveMainLoop();
-        }
-}
-
-void mytime(int value) {
-        // выводим количество кадров которые успели нарисовать за 1 секунду
-        std::cerr << "fps= " << fps << std::endl;
-        fps=0; // обнуление счетчика
-        glutTimerFunc(1000, mytime, 0); // следующий вызов
-}
-
-int main(int argc, char** argv) {
-        glutInit (&argc,argv); // инициализация glut
-        // будем использовать двойную буферизацию, цветовое пространство RGB,
-        // сортировку по глубине aka Z-буфер
-        glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB|GLUT_DEPTH);
-        // размер окна пусть будет 800 на 600
-        glutInitWindowSize( 800,600);
-        // создаем окно, на выходе получаем идентификатор созданного окна
-        int glWin=glutCreateWindow("My program");
-        init(); // подготовка к рисованию
-        glutDisplayFunc(display); // рисование
-        glutReshapeFunc(reshape); // действия при изменении окна
-        glutKeyboardFunc(key);   // обработка нажатий клавиш
-        // запускаем таймер м периодичностью 1 раз в секунду
-        glutTimerFunc(1000, mytime, 0);
-        glutIdleFunc(display); // анимация
-        glutMainLoop(); // главный цикл glut
-
-        return 0;
 }
